15f03836dcb997a4042f32ff564aaf7f5c64b523
Sergio Gurillo <sergio.gurillo11@gmail.com>
2024-08-21 19:04:48 +0200

    update images & status

--- core/src/main/java/com/guuri11/bbal3x3/player/PlayerStatus.java
+++ core/src/main/java/com/guuri11/bbal3x3/player/PlayerStatus.java
@@ -1,11 +1,16 @@
 package com.guuri11.bbal3x3.player;
 
 public enum PlayerStatus {
+  DRIBBLE("Dribble"),
   IDLE("Idle"),
+  IDLE_WITH_BALL("Idle_With_Ball"),
   JUMP_SHOT("Jump_Shoot"),
+  JUMP_SHOT_WITH_BALL("Jump_Shoot_With_Ball"),
   PASS_RECEIVE("Pass_Receive"),
+  PASS_RECEIVE_WITH_BALL("Pass_Receive_With_Ball"),
   RUN("Run"),
-  STAND_SHOOT("Stand_Shoot");
+  STAND_SHOOT("Stand_Shoot"),
+  STAND_SHOOT_WITH_BALL("Stand_Shoot_With_Ball");
 
   public final String value;
 
